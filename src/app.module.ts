7bd968dad1945903f0ccf95a843198af91f07ae1
Alexis Huayna <xt10125@rimac.com.pe>
2025-11-25 17:22:52 -0500

    chore: add env configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  })],
   controllers: [],
   providers: [AppService],
 })
