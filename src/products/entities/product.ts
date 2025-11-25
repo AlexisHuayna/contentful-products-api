bca30457e1df82aff4f5f064459bd625151496b7
Alexis Huayna <xt10125@rimac.com.pe>
2025-11-25 18:38:03 -0500

    refactor: product entiy

--- src/products/entities/product.ts
+++ src/products/entities/product.ts
@@ -1,31 +1,58 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity()
+@Entity({ name: 'products' })
 export class Product {
-    @PrimaryGeneratedColumn()
-    id: number;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-    @Column()
+    @Index()
+    @Column({ type: 'varchar', unique: true})
     externalId: string;
 
-    @Column()
+    @Column({ type: 'int', nullable: true})
+    sku: number;
+
+    @Column({ type: 'varchar'})
     name: string;
 
-    @Column()
+    @Column({ type: 'varchar', nullable: true })
+    brand: string;
+
+    @Index()
+    @Column({ type: 'varchar', nullable: true })
+    model: string;
+
+    @Index()
+    @Column({ type: 'varchar', nullable: true })
     category: string;
 
-    @Column()
-    price: number;
+    @Column({ type: 'varchar', nullable: true })
+    color: string;
 
-    @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+    @Column({ type: 'numeric', nullable: true })
+    price: number | null;
+
+    @Column({ type: 'varchar', nullable: true })
+    currency: string | null;
+
+    @Column({ type: 'int', nullable: true })
+    stock: number | null;
+
+    @Column({ type: 'timestamptz', nullable: true })
+    contentCreatedAt: Date;
+    
+    @Column({ type: 'timestamptz', nullable: true })
+    contentUpdatedAt: Date;
+
+    @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
 
-    @UpdateDateColumn({ default: () => 'CURRENT_TIMESTAMP' })
+    @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
     updatedAt: Date;
 
-    @Column({ default: false })
+    @Column({ type: 'boolean', default: false })
     deleted: boolean;
 
-    @DeleteDateColumn({ nullable: true })
-    deletedAt: Date;
+    @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+    deletedAt: Date | null;
 }
