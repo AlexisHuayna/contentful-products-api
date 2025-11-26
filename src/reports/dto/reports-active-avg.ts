38c0823317008f8f36ed0638ed4bf239ef8680b1
Alexis Huayna <xt10125@rimac.com.pe>
2025-11-26 14:52:03 -0500

    feat: documentation Dtos, routes and responses

--- src/reports/dto/reports-active-avg.ts
+++ src/reports/dto/reports-active-avg.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsBoolean, IsDate, IsNotEmpty, IsOptional } from "class-validator";
 
@@ -5,15 +6,18 @@ export class ReportsActiveAvg {
     @Type(() => Boolean)
     @IsBoolean()
     @IsOptional()
+    @ApiProperty({ description: 'Include average price flag', required: false, default: false })
     withPrice: boolean;
 
     @Type(() => Date)
     @IsDate()
     @IsNotEmpty()
+    @ApiProperty({ description: 'Start date', required: true })
     startDate: Date;
 
     @Type(() => Date)
     @IsDate()
     @IsNotEmpty()
+    @ApiProperty({ description: 'End date', required: true })
     endDate: Date;
 }
