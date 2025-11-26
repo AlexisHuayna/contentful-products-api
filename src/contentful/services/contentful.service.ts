a2c0f3b4c7c3182a881a20b4b2a05e8cf54e1f98
Alexis Huayna <xt10125@rimac.com.pe>
2025-11-25 20:09:24 -0500

    feat: implement contentful client

--- src/contentful/services/contentful.service.ts
+++ src/contentful/services/contentful.service.ts
@@ -1,18 +1,19 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+import { ContentfulProductItem, ContentfulProductsResponse } from 'src/types/contentful-product.interface';
 
 @Injectable()
 export class ContentfulService {
 
-    private readonly CONTENTFUL_SPACE_ID: string;
-    private readonly CONTENTFUL_ACCESS_TOKEN: string;
-    private readonly CONTENTFUL_ENVIRONMENT: string;
-    private readonly CONTENTFUL_CONTENT_TYPE: string;
+    constructor(private readonly httpService: HttpService) {
 
-    constructor(private readonly configService: ConfigService) {
-        this.CONTENTFUL_SPACE_ID = this.configService.getOrThrow<string>('CONTENTFUL_SPACE_ID');
-        this.CONTENTFUL_ACCESS_TOKEN = this.configService.getOrThrow<string>('CONTENTFUL_ACCESS_TOKEN');
-        this.CONTENTFUL_ENVIRONMENT = this.configService.getOrThrow<string>('CONTENTFUL_ENVIRONMENT');
-        this.CONTENTFUL_CONTENT_TYPE = this.configService.getOrThrow<string>('CONTENTFUL_CONTENT_TYPE');
+    }
+
+    async fetchProducts(): Promise<ContentfulProductsResponse> {
+        const response = await firstValueFrom(
+            this.httpService.get<ContentfulProductsResponse>('/entries')
+        );
+        return response?.data;
     }
 }
