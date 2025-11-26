bf4105683d267f6a57a747f6ada53772761d543f
Alexis Huayna <xt10125@rimac.com.pe>
2025-11-25 21:16:47 -0500

    feat: generating migrations

---
+++ src/database/data-source.ts
@@ -0,0 +1,22 @@
+// src/database/data-source.ts
+import 'reflect-metadata';
+import { Product } from '../products/entities/product';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({
+    path: resolve(process.cwd(), '.env'),
+});
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT ?? 5432),
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'products_db',
+  entities: [Product],
+  migrations: [__dirname + '/migrations/*.ts'],
+  synchronize: false,
+});
