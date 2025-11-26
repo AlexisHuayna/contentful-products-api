7df835aadd7ff757bd111ee30b9d4f25a1fdb500
Alexis Huayna <xt10125@rimac.com.pe>
2025-11-26 12:07:49 -0500

    feat: auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthLoginDto } from './dto/auth-login';
 
-@Controller('auth')
-export class AuthController {}
+@Controller({
+    path: 'auth',
+    version: '1',
+})
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() loginDto: AuthLoginDto): Promise<{ accessToken: string }> {
+        const user = await this.authService.validateUser(loginDto.username, loginDto.password);
+
+        return this.authService.login(user);
+    }
+}
